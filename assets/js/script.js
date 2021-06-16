fe54a0ce30d0e279ca095e365eecbeb14a44b2f1
bengallagher <bgallag5@gmail.com>
2021-06-16 15:08:54 -0400

    q's display and iterate, and store answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,88 +2,68 @@
 var questions= [
     { q: `It's over Anakin I have the ______.`,  
       a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'},
+      correct: '2'},
 
-      { q: `Which of the following characters was NOT played by Mike Myers in Austin Powers?.`,  
-      a: ["Austin Powers", "Dr. Evil", "Fat Bastard", "Number Two"], 
-      correct: 'b'},
+    { q: `The sky is ______.`,  
+      a: ["blue", "green", "red", "yelow"], 
+      correct: '1'},
 
-      { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'},
-
-      { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'},
-
-      { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'},
-
-      { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'},
-
-      { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'},
-
-      { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'},
-
-      { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
-      correct: 'b'}
+    { q: `The most important meal of the day ______.`,  
+      a: ["lunch", "second breakfast", "snacks", "breakfast"], 
+      correct: '4'},     
 ];
 
 var score = 0;
 var userAnswer = "";
 var questionBox = document.getElementById('questions');
 var buttonBox = document.getElementById("mybuttons");
-// var aSelection = document.getElementById('aButton');
-// var bSelection = document.getElementById('bButton');
-// var cSelection = document.getElementById('cButton');
-// var dSelection = document.getElementById('dButton');
 var buttonEl = document.getElementsByClassName('.mybuttons')
 var soundCorrect = "";
 var soundWrong = "";
-
+var questionNumber = 0;
+var currentQuestion = "";
+var answerChoices = "";
 
 displayQuestion = function (){ //var startScreen hide, call timer, call getQuestion. no other logic in playGame- else goes into displYQuestion or compareAnswers
      //have start button that playGame(); and is hidden afterwards 
 
-   var questionNumber = 0;
+   
    if (questionNumber < questions.length){
-       var currentQuestion = document.createElement('h3');
+        currentQuestion = document.createElement('h3');
        currentQuestion.classList.add('mybuttons');
        currentQuestion.textContent = questions[questionNumber].q;
        questionBox.appendChild(currentQuestion);
 
-       for(i = 0; i < questions[i].a.length; i++){
-           var answerChoices = document.createElement('button');
+       for(i = 0; i < questions[questionNumber].a.length; i++){
+            answerChoices = document.createElement('button');
            answerChoices.classList.add('btn-success', 'btn')  //get button id's/value taskinator module edit and delete id's?
            answerChoices.setAttribute("id", i + 1);  //asigns unique ID to each button 
            answerChoices.innerHTML = ((i+1) + ". " + questions[questionNumber].a[i]);
            answerChoices.addEventListener('click', handleAnswer);
            buttonBox.appendChild(answerChoices);
-           console.log(questionNumber);
-           console.log(i);
        }
      
    }
-   answerChoices.addEventListener('click', function(event) {
-    console.log(event.target.id);
-  });
 
-}
-
+};
 
   var handleAnswer = function (event) {
     console.log(event.target.id);
+    if (event.target.id === questions[questionNumber].correct){
+        console.log('correct!');
+    } else{
+        console.log('incorrect');
+    }
+    displayNext();
    }
 
 
+   var displayNext = function (){
+    questionBox.innerHTML="";
+    buttonBox.innerHTML="";
+questionNumber++;
+displayQuestion();
+   }
 // document.querySelectorAll('.btn').forEach(function(el){
 // el.addEventListener('click', function(){
 //     console.log(this.id);
