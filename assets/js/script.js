1cfb312a94793f4315c95fb19e222b6022d31650
bengallagher <bgallag5@gmail.com>
2021-06-19 20:06:14 -0400

    finished project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,67 @@
 // TODO: Create an array with five question objects
 var questions= [
-    { q: `It's over Anakin I have the ______.`,  
-      a: ["lightsaber", "high ground", "force", "deathstar"], 
+    { q: `"It's over Anakin I have the ______."`,  
+      a: ["Lightsaber", "High ground", "Force", "Deathstar"], 
       correct: '2'},
 
-    { q: `The sky is ______.`,  
-      a: ["blue", "green", "red", "yelow"], 
-      correct: '1'},
+    { q: `In episode 3, Anakin turned to the dark side searching for the power to protect:`,  
+      a: ["His Mother", "The Jedi Order", "Padme", "Princess Leia"], 
+      correct: '3'},
 
-    { q: `The most important meal of the day ______.`,  
-      a: ["lunch", "second breakfast", "snacks", "breakfast"], 
-      correct: '4'},     
-];
+    { q: `All of the following are Sith lords except:`,  
+      a: ["Count Dooku", "Darth Sidious", "General Grevious", "Darth Maul"], 
+      correct: '3'},     
+
+      { q: `Chewbacca is a member of what species?`,  
+      a: ["Gungan", "Wookie", "At-at", "Jawa"], 
+      correct: '2'}, 
+
+      { q: `Which Jedi Master carried a purple lightsaber, instead of a traditional green or blue one?`,  
+      a: ["Darth Maul", "Anakin Skywalker", "Qui Gon Jinn", "Mace Windu"], 
+      correct: '4'}, 
+
+      { q: `What is Luke Skywalker's home planet?`,  
+      a: ["Tatooine", "Endor", "Hoth", "Earth"], 
+      correct: '1'}, 
+
+      { q: `A 'fan-cut' of Episodes 1 and 2 edited out every line of dialog from THIS character:`,  
+      a: ["Jaba the Hut", "Padme", "C3PO", "Jar Jar Binks"], 
+      correct: '4'}, 
+    ];
 
 var score = 0;
 var userAnswer = "";
 var questionBox = document.getElementById('questions');
 var buttonBox = document.getElementById("mybuttons");
-var buttonEl = document.getElementsByClassName('.mybuttons')
-var soundCorrect = "";
-var soundWrong = "";
+var buttonEl = document.getElementsByClassName('.mybuttons');
+var playAgain = document.getElementById('playAgain')
+var scoreCard = document.getElementById('scorebox');
+var timer = document.getElementById('timer');
+var soundCorrect = new Audio("assets/images_sounds/Happy_Confirmation.mp3");
+var soundWrong = new Audio("assets/images_sounds/upsetr2.mp3")
+var lightsaber = new Audio("assets/images_sounds/lightsaber_03.wav")
 var questionNumber = 0;
-var currentQuestion = "";
-var answerChoices = "";
+var timeLeft;
+var currentQuestion;
+var answerChoices;
+var finalStats;
 
-displayQuestion = function (){ //var startScreen hide, call timer, call getQuestion. no other logic in playGame- else goes into displYQuestion or compareAnswers
+
+$('#startButton').on("click", function(){
+$('.openScreen').hide();
+$('#gameDisplay').show();
+playGame();
+lightsaber.play();
+})
+
+playGame = function (){ //var startScreen hide, call timer, call getQuestion. no other logic in playGame- else goes into displYQuestion or compareAnswers
      //have start button that playGame(); and is hidden afterwards 
+     questionBox.style.display = "block";
+     timer.style.display = "block";
+     playAgain.style.display = "none";
+     scoreCard.style.display = "none";
+     timeLeft = 60;
+     countDown();
 
    
    if (questionNumber < questions.length){
@@ -36,23 +72,29 @@ displayQuestion = function (){ //var startScreen hide, call timer, call getQuest
 
        for(i = 0; i < questions[questionNumber].a.length; i++){
             answerChoices = document.createElement('button');
-           answerChoices.classList.add('btn-success', 'btn')  //get button id's/value taskinator module edit and delete id's?
+           answerChoices.classList.add('btn-success', 'btn', 'col-md-5')  //get button id's/value taskinator module edit and delete id's?
            answerChoices.setAttribute("id", i + 1);  //asigns unique ID to each button 
            answerChoices.innerHTML = ((i+1) + ". " + questions[questionNumber].a[i]);
            answerChoices.addEventListener('click', handleAnswer);
            buttonBox.appendChild(answerChoices);
        }
-     
    }
-
+   
+   endGame();
 };
 
   var handleAnswer = function (event) {
+      var chosenAnswer = event.target.getAttribute('id')
+      
     console.log(event.target.id);
-    if (event.target.id === questions[questionNumber].correct){
-        console.log('correct!');
+    if (chosenAnswer === questions[questionNumber].correct){
+        score++;
+
+        soundCorrect.play();
+        alert('Thats Correct!');
     } else{
-        console.log('incorrect');
+      soundWrong.play();
+        alert('Incorrect')
     }
     displayNext();
    }
@@ -62,57 +104,48 @@ displayQuestion = function (){ //var startScreen hide, call timer, call getQuest
     questionBox.innerHTML="";
     buttonBox.innerHTML="";
 questionNumber++;
-displayQuestion();
+playGame();
    }
-// document.querySelectorAll('.btn').forEach(function(el){
-// el.addEventListener('click', function(){
-//     console.log(this.id);
-//     //call check answer function
-// })
-// })
 
 
-
-// $('.mybuttons').on("click", function(){
-//     var userAnswer = $('.btn').attr("id");
-//     console.log(userAnswer);
-// })
-
-// $('questions').on("click", "btn", function(){
-//     var userAnswer = $(this).attr("id");
-//     console.log(userAnswer);
-// })
-
-// var allButtons = document.getElementsByClassName("btn")
-
-
-// playGame = function(){ //get question and display it on the screen  //if{ no more questions, end quiz and show score.} else{
    
-//     for (i=0; i < questions.length; i++){
-//       var newQuestion= questions[i];
-//       var correctAnswer = newQuestion.correct;
-//         questionBox.innerHTML = newQuestion.q; 
+   var gameTimer = setInterval(countDown, 1000); 
+function countDown(){
+  document.getElementById('timer').innerHTML = (timeLeft + " Seconds Remaining")
+  if(timeLeft > 0){
+  timeLeft--;
+  } else if(timeLeft <= 0){
+    displayNext();
+  }
+  
+}
 
-        // document.addEventListener
-        // $("mybuttons").on("click", "button", function(){
-        //     var userAnswer = $(this)
-        //     .attr("id");
-        //     console.log(userAnswer);
-        //     console.log("A")
-    
-        //     if (userAnswer === correctAnswer){
-        //         console.log(userAnswer, "Correct", index);
-        //         score++;
-        //     }
-        //     else {
-        //         console.log(userAnswer, 'incorrect');
-        //     }
-        //     });
-        
-// }
-// }
+function endGame(){
+  if (questionNumber == questions.length){
+    clearInterval(gameTimer);
+    questionBox.style.display = "none";
+    timer.style.display = "none";
+    playAgain.style.display = "block";
+    scoreCard.style.display = "block";
+    buttonBox.innerHTML="";
+    var finalStats = document.createElement('h3')
+    finalStats.innerHTML = "You finished with a score of " + score + "/" + questions.length;
+    scoreCard.appendChild(finalStats);
+  } 
+}
 
-displayQuestion();
+$('#playAgain').on("click", function(){
+  score = 0;
+  questionNumber = 0;
+  scoreCard.innerHTML = "";
+  scoreCard.style.display = "none";
+  gameTimer = setInterval(countDown, 1000);
+  lightsaber.play();
+  // questionBox.innerHTML = "";
+  playGame();
+})
+
+
 
 
 // /////////////////NOTES//////////////////NOTES///////////////////////NOTES//////////////NOTES////////////////////////////////////////////////////////////////////////////////////////////
@@ -121,10 +154,13 @@ displayQuestion();
 //questions:
 //how do i iterate through my questions? use a for loop or appendChild at end of each question?
 //
+////use display: none to hide elements on click start button, create start page  'div'.style.display = "none";
 //
+//TODO:::::
+//create results screen 
 //
-//
-//
+//if (no more questions){showResults()}
+//if timer hits 0 end quiz
 //
 //
 //
